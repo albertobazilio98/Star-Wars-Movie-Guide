7a74971c097a31faa24cb7623d1abd6e493b2ff9
albertobazilio98 <albertobazilio98@gmail.com>
2019-06-10 13:20:17 -0400

    Correções, estilos e opening crawl
    correção da exibição de personagens, veiculos, planestas, especies e espaço naves.
    adição do opening crawl animado

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     >
     <div>
       <v-toolbar color="primary">
-        <v-toolbar-title> Star Wars </v-toolbar-title>
+        <v-toolbar-title> Star Wars Movie Guide </v-toolbar-title>
         <v-spacer></v-spacer>
       </v-toolbar>
       <div>
@@ -72,6 +72,14 @@
                           class="grey darken-4"
                         ></v-img>
                       </v-card>
+                      <div class="text-xs-center">
+                        <v-btn
+                          round
+                          outline
+                          color="primary"
+                          @click="openingCrawl = true"
+                        >Opening Crawl</v-btn>
+                      </div>
                     </v-flex>
 
                     <v-flex xs12 md6>
@@ -91,22 +99,105 @@
                       
                       <v-card>
                         <v-expansion-panel>
-                          <v-expansion-panel-content style="background-color: #9E9E9E">
+                          <v-expansion-panel-content style="background-color: #9E9E9E; color: #FAC223">
                             <template v-slot:header>
-                              titulo
+                              Characters
                             </template>
                             <v-data-table
-                              :items="movie.characterName"
+                              :items="movie.charactersNames"
                               hide-headers
+                              hide-actions
                             >
                               <template v-slot:items="props">
-                                <tr style="background-color: #9E9E9E">
-                                  <td>{{ props.item.name }}</td>
+                                <tr style="background-color: #E0E0E0">
+                                  <td>{{ props.item.data.name }}</td>
                                 </tr>
                               </template>
                             </v-data-table>
-                               
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                      </v-card>
 
+                      <v-card>
+                        <v-expansion-panel>
+                          <v-expansion-panel-content style="background-color: #9E9E9E">
+                            <template v-slot:header>
+                              Planets
+                            </template>
+                            <v-data-table
+                              :items="movie.planetsNames"
+                              hide-headers
+                              hide-actions
+                            >
+                              <template v-slot:items="props">
+                                <tr style="background-color: #E0E0E0">
+                                  <td>{{ props.item.data.name }}</td>
+                                </tr>
+                              </template>
+                            </v-data-table>
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                      </v-card>
+
+                      <v-card>
+                        <v-expansion-panel>
+                          <v-expansion-panel-content style="background-color: #9E9E9E">
+                            <template v-slot:header>
+                              Starships
+                            </template>
+                            <v-data-table
+                              :items="movie.starshipsNames"
+                              hide-headers
+                              hide-actions
+                            >
+                              <template v-slot:items="props">
+                                <tr style="background-color: #E0E0E0">
+                                  <td>{{ props.item.data.name }}</td>
+                                </tr>
+                              </template>
+                            </v-data-table>
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                      </v-card>
+
+                      <v-card>
+                        <v-expansion-panel>
+                          <v-expansion-panel-content style="background-color: #9E9E9E">
+                            <template v-slot:header>
+                              Vehicles
+                            </template>
+                            <v-data-table
+                              :items="movie.vehiclesNames"
+                              hide-headers
+                              hide-actions
+                            >
+                              <template v-slot:items="props">
+                                <tr style="background-color: #E0E0E0">
+                                  <td>{{ props.item.data.name }}</td>
+                                </tr>
+                              </template>
+                            </v-data-table>
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                      </v-card>
+
+                      <v-card>
+                        <v-expansion-panel>
+                          <v-expansion-panel-content style="background-color: #9E9E9E">
+                            <template v-slot:header>
+                              Species
+                            </template>
+                            <v-data-table
+                              :items="movie.speciesNames"
+                              hide-headers
+                              hide-actions
+                            >
+                              <template v-slot:items="props">
+                                <tr style="background-color: #E0E0E0">
+                                  <td>{{ props.item.data.name }}</td>
+                                </tr>
+                              </template>
+                            </v-data-table>
                           </v-expansion-panel-content>
                         </v-expansion-panel>
                       </v-card>
@@ -114,6 +205,21 @@
                     </v-flex>
                   </v-layout>
                 </v-container>
+              </v-card>
+            </v-dialog>
+
+            <v-dialog v-model="openingCrawl" max-width="800px">
+              <v-card color="black" class=".card" height="400px">
+                <v-icon color="primary" @click="openingCrawl=false">close_rounded</v-icon>
+                <section class="star-wars">
+                  <div class="crawl">
+                    <div class="title">
+                      <p>Episode {{ roman[movie.espisode_id] }}</p>
+                      <h1>{{ movie.title }}</h1>
+                    </div>
+                    <p>{{ movie.opening_crawl }}</p> 
+                  </div>
+                </section>
               </v-card>
             </v-dialog>
           </v-layout>
@@ -126,33 +232,54 @@
 
 <script>
 import find from "@/services/api-service";
+import axios from "axios";
 
 export default {
   name: 'app',
   data: () => ({
+    openingCrawl: false,
     search: "",
-    languageDialog: true,
     detailsDialog: false,
     headers: [
-      { text: "Episode",   value: "episode", width: "10" },
+      { text: "Episode",   value: "episode", width: "10", sortable: false},
       { text: "Title",     value: "title"   },
-      { text: "",          value: "actions" }
+      { text: "",          value: "actions", sortable: false }
     ],
     movie: {},
     movies: [],
-    roman: ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
+    roman: ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII"],
+    characters: [],
+    aux: {
+      characters: [],
+      planets: [],
+      starships: [],
+      vehicles: [],
+      species: []
+    }
   }),
   methods: {
+    fixPromises() {
+      this.characters = this.movie.characterName;
+    },
     openDetailsDialog (item) {
       this.movie = item;
-      this.movie.characterName = [{name: "zé"}];
+      this.movie.charactersNames = [];
+      this.movie.planetsNames = [];
+      this.movie.starshipsNames = [];
+      this.movie.vehiclesNames = [];
+      this.movie.speciesNames = [];
       this.movie.poster = require(`@/images/posters/${this.movie.episode_id}.jpg`);
       this.detailsDialog = !this.detailsDialog;
       this.findCharacter();
-      this.movie.characterName.push({name:"joao"})
-    },
-    openLanguageDialog () {
-      this.languageDialog = !this.languageDialog;
+      this.movie.charactersNames = this.aux.characters;
+      this.findPlanet();
+      this.movie.planetsNames = this.aux.planets;
+      this.findStarships();
+      this.movie.starshipsNames = this.aux.starships;
+      this.findVehicles();
+      this.movie.vehiclesNames = this.aux.vehicles;
+      this.findSpecie();
+      this.movie.speciesNames = this.aux.species;
     },
     async listMovies() {
       try {
@@ -163,21 +290,64 @@ export default {
       }
     },
     async findCharacter() {
-      var aux = [];
+      this.aux.charactersNames = [];
       for(var i in this.movie.characters) {
-        try {
-          let response = await find(this.movie.characters[i].slice(21));
-          console.log(response.data)
-          aux.push(response.data);
-        } catch {
-          this.movie.characterName.push("error");
-        }
-        /*find(this.movie.characters[i].slice(21))
-        .then(function (response) {
-          this.movie.characterName.push(response.data);
-        })*/
+        find(this.movie.characters[i].slice(21))
+        .then(response => (
+          this.aux.characters.push(response)
+        ))
+        .catch(error => (
+          console.log(error)
+        ))
       }
-      this.movie.characterName = aux;
+    },
+    async findPlanet() {
+      this.aux.planetsNames = [];
+      for(var i in this.movie.planets) {
+        find(this.movie.planets[i].slice(21))
+        .then(response => (
+          this.aux.planets.push(response)
+        ))
+        .catch(error => (
+          console.log(error)
+        ))
+      }
+    },
+    async findStarships() {
+      this.aux.starshipsNames = [];
+      for(var i in this.movie.starships) {
+        find(this.movie.starships[i].slice(21))
+        .then(response => (
+          this.aux.starships.push(response)
+        ))
+        .catch(error => (
+          console.log(error)
+        ))
+      }
+    },
+    async findVehicles() {
+      this.aux.vehiclesNames = [];
+      for(var i in this.movie.vehicles) {
+        find(this.movie.vehicles[i].slice(21))
+        .then(response => (
+          this.aux.vehicles.push(response)
+        ))
+        .catch(error => (
+          console.log(error)
+        ))
+      }
+    },
+    async findSpecie() {
+      this.aux.speciesNames = [];
+      for(var i in this.movie.species) {
+        find(this.movie.species[i].slice(21))
+        .then(response => (
+          this.aux.species.push(response)
+        ))
+        .catch(error => (
+          console.log(error)
+        ))
+      }
     }
   },
   created() {
@@ -185,6 +355,58 @@ export default {
   }
 };
 </script>
+
+<style>
+.card {
+  width: 100%;
+  height: 100%;
+  background: #000;
+}
+
+.star-wars {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  height: 800px;
+  color: #feda4a;
+  font-family: 'Pathway Gothic One', sans-serif;
+  font-size: 500%;
+  font-weight: 600;
+  letter-spacing: 6px;
+  line-height: 150%;
+  perspective: 400px;
+  text-align: justify;
+}
+
+.crawl {
+  position: relative;
+  top: 99999px;
+  transform-origin: 50% 100%;
+  animation: crawl 60s linear;
+}
+
+.crawl > .title {
+  font-size: 90%;
+  text-align: center;
+}
+
+.crawl > .title h1 {
+  margin: 0 0 100px;
+  text-transform: uppercase;
+}
+
+@keyframes crawl {
+	0% {
+    top: -100px;
+    transform: rotateX(30deg)  translateZ(0);
+  }
+	100% { 
+    top: -6000px;
+    transform: rotateX(25deg) translateZ(-2500px);
+  }
+}
+</style>
+
 
 
 
